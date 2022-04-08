2da37bd9fb9ca9754ab9bbdcf24781b1a5377e90
Alex <alex_tr1992@hotmail.com>
2022-04-08 15:44:20 -0300

    ajuste no mapeamento

--- src/main/java/com/getdata/dataprovider/entity/FeesLoanEntity.java
+++ src/main/java/com/getdata/dataprovider/entity/FeesLoanEntity.java
@@ -1,23 +1,9 @@
 package com.getdata.dataprovider.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.FieldNameConstants;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -35,7 +21,7 @@ public class FeesLoanEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToMany(mappedBy = "feesPersonalLoan", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "feesLoan", cascade = CascadeType.ALL, orphanRemoval = true)
     @Setter
     private List<PriorityServiceEntity> services;
 
